23632f83d1461224ad0d6b1d85411b7f4dce08b3
Yoshihiro <b1014225@fun.ac.jp>
2020-01-18 22:16:40 +0900

    first commit

---
+++ src/main/java/com/dededesignworkshop/seisankun_api/SeisankunApiApplication.java
@@ -0,0 +1,13 @@
+package com.dededesignworkshop.seisankun_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeisankunApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeisankunApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dededesignworkshop/seisankun_api/SeisankunApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dededesignworkshop.seisankun_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeisankunApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
