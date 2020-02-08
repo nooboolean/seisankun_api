2c55aa0d7537876461860ddab4dd5c7c35a05c71
nooboolean <nooboolean@i.softbank.jp>
2020-02-08 16:02:27 +0900

    決済情報の詳細を取得する参照エンドポイント作成

--- src/main/java/com/dededesignworkshop/seisankun_api/infrastructure/repository/PaymentRepository.java
+++ src/main/java/com/dededesignworkshop/seisankun_api/infrastructure/repository/PaymentRepository.java
@@ -6,9 +6,13 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface PaymentRepository {
     @Select("SELECT * FROM payments WHERE travel_id = #{travel_id}")
     List<PaymentEntity> findByTravelId(@Param("travel_id") Integer travel_id);
+
+    @Select("SELECT * FROM payments WHERE id = #{payment_id}")
+    Optional<PaymentEntity> findByPaymentId(@Param("payment_id") Integer payment_id);
 }
