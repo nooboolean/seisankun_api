1c16fb29b598dfe96359ea8484b7873117e75d2c
nooboolean <nooboolean@i.softbank.jp>
2020-03-07 04:47:46 +0900

    travelにjoinするときに旅行者間の立て替え関係のレコードも作成する

---
+++ src/main/java/com/dededesignworkshop/seisankun_api/infrastructure/entity/BorrowMoneyEntity.java
@@ -0,0 +1,37 @@
+package com.dededesignworkshop.seisankun_api.infrastructure.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowMoney {
+
+    private Integer id;
+
+    private Integer travelId;
+
+    private Integer borrowerId;
+
+    private Integer lenderId;
+
+    private Integer money;
+
+    private Integer createdBy;
+
+    private String createdAt;
+
+    private Integer updatedBy;
+
+    private String updatedAt;
+
+    private Integer deletedBy;
+
+    private String deletedAt;
+
+    private Integer deleteFlag;
+}
